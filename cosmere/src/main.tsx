d2f1319440730723217ffef5f9f3291fbda77654
thadeucupello <thadeu.cupello@ediouro.com.br>
2026-07-20 17:31:45 -0300

    Carrega ajustes finais de mobile e capas

--- cosmere/src/main.tsx
+++ cosmere/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import './styles/global.css';
 import './styles/header.css';
 import './styles/sections.css';
+import './styles/mobile-fixes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
